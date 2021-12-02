5c5e99f011239a11ee497aa8f932c344a9b00892
Sam Williams <swilliams@mastercontrol.com>
2021-12-01 19:24:47 -0700

    Update to read depth, calculate increase or decrease, and display output and final answer

--- day1/depthtracker.go
+++ day1/depthtracker.go
@@ -3,17 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"strconv"
 )
 
 func check(e error) {
-	fmt.Print("fail")
 	if e!= nil {
 		panic(e)
 	}
 }
 
 func main() {
-	podata, err := os.ReadFile("input.txt")
+	podatafile, err := os.Open("input.txt")
 	check(err)
-	fmt.Print(string(podata))
+	defer podatafile.Close()
+
+	scanner := bufio.NewScanner(podatafile)
+	previousDepth := -1
+	depthIncreases := 0
+
+	for scanner.Scan() {
+		depth, err := strconv.Atoi(scanner.Text())
+		check(err)
+		str := ""
+		if previousDepth < 0 {
+			str = "(N/A - no previous measurement)"
+		} else {
+			if previousDepth < depth {
+				str = "Increases"
+				depthIncreases++
+			} else {
+				str = "Decreases"
+			}
+		}
+		fmt.Printf("%d (%s)\n", depth, str)
+		previousDepth = depth
+	}
+	fmt.Printf("Depth increased %d times", depthIncreases)
 }
